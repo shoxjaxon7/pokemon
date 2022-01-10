40b6584e415a221c754746ab064bbe267e3cffe0
shoxjaxon7 <shox652xolmurodov@gmail.com>
2022-01-10 14:21:47 +0500

    intianl commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+//  SELECT ELEMEN IN HTML
+let elItem = document.querySelector(".main__item");
+
+//  STARTED WITH FOR OF
+for (let pokemon of pokemons) {
+  //  CREATE ELEMENT
+  let newTopDiv = document.createElement("div");
+  let newImg = document.createElement("img");
+  let newdivTitle = document.createElement("div");
+  let newHead = document.createElement("h4");
+  let newDescForWidth = document.createElement("p");
+  let newDivButton = document.createElement("div");
+  let newBottonDesc = document.createElement("h3");
+
+  //  AFTER CREATE ELEMENT  ADD CLASS
+  newTopDiv.classList.add("card");
+  newImg.setAttribute("src", pokemon.img);
+  newdivTitle.classList.add("card-body");
+  newDescForWidth.classList.add("card-text");
+  newDivButton.classList.add("last-group");
+  newBottonDesc.classList.add("last__desc");
+  // ADDED TEXT CONTENT
+  newHead.textContent = pokemon.name;
+  newDescForWidth.textContent = pokemon.avg_spawns;
+  newBottonDesc.textContent = pokemon.candy;
+  //APPEND CHILD //
+  elItem.appendChild(newTopDiv);
+  newTopDiv.appendChild(newImg);
+  newTopDiv.appendChild(newdivTitle);
+  newdivTitle.appendChild(newHead);
+  newdivTitle.appendChild(newDescForWidth);
+  newdivTitle.appendChild(newDivButton);
+  newDivButton.appendChild(newBottonDesc);
+}
